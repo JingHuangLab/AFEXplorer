d8f9885d9c009c4b7bd6dbcf4bef3796c01cf4e2
Jing Huang <jing-huang@users.noreply.github.com>
2023-09-26 19:00:57 +0800

    Update run_afexplore_optim.sh

--- scripts/run_afexplore_optim.sh
+++ scripts/run_afexplore_optim.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 feat_dir=$1 # Directory of features.pkl and output
-target_state=$2 # 'kinase', 'IOMemP', 'ADK'
-protein_type=$3 # "in" or "out"
+protein_type=$2 # 'kinase', 'IOMemP', 'ADK'
+target_state=$3 # "in" or "out"
 afparam_dir=$4 # Directory of AF model parameters
 
 nsteps=500 # Max number of iterations
@@ -12,7 +12,7 @@ learning_rate=0.0001
 
 python ../afexplore/afexplore_optim.py \
        --rawfeat_dir $feat_dir \
-       --output_dir $seq_dir \
+       --output_dir $feat_dir \
        --afparam_dir $afparam_dir \
        --nclust $nclust \
        --nsteps $nsteps \
